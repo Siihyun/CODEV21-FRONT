1ac2c591f7cd44339afc45effe877cbd966caef7
kkimke <k_k_eun@naver.com>
2021-03-12 03:48:59 +0900

    header 디자인 수정

--- modules/project.js
+++ modules/project.js
@@ -1,4 +1,17 @@
-const handleProject = () => {};
+const handleProject = () => {
+  const topic = document.querySelector(".topic"),
+    team = document.querySelector(".team"),
+    content = document.querySelector(".content");
+
+  fetch('https:', {
+        method: "GET",
+        body: JSON.stringify(_data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+};
 
 module.exports = {
   handleProject,
