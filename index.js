b2e8b4051144491942a0eaa2bc44570b1d241fbe
changmink <changmin043@gmail.com>
2021-03-11 23:52:21 +0900

    [#1] 로그인 버그 수정

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("./css/about.css");
 const { handleHome } = require("./modules/home.js");
 const { handleAbout } = require("./modules/about.js");
 const { handleProject } = require("./modules/project.js");
-const { initEditor } = require("./modules/edit.js");
+const { handleEdit } = require("./modules/edit.js");
 const { handleLogin } = require("./modules/login.js");
 const { handleJoin } = require("./modules/join.js");
 
@@ -26,9 +26,33 @@ window.onload = () => {
     element.addEventListener("click", (event) => {
       const pathName = event.target.getAttribute("route");
       historyRouterPush(pathName, contentDiv);
+      if(pathName === "/edit") {
+        handleEdit()
+      }
+      if(pathName === "/login") {
+        handleLogin()
+      }
     });
   });
+  
 };
+
+if (localStorage.getItem('j2kb-accessToken')) {
+  const login = document.querySelector("#login")
+  const edit = document.querySelector("#edit")
+  const join = document.querySelector("#join")
+
+  login.textContent = "Logout"
+  edit.classList.remove("hide")
+  join.classList.add("hide")
+
+  login.addEventListener("click", e => {
+    localStorage.removeItem("j2kb-accessToken");
+    login.textContent = "Login"
+    edit.classList.add("hide")
+    join.classList.remove("hide")
+  })
+}
 /* 원하는 event를 여기다 다시면 됩니다 */
 contentDiv.addEventListener("click", (e) => {
   const target = e.target;
@@ -36,9 +60,6 @@ contentDiv.addEventListener("click", (e) => {
   if (target.classList.contains("test")) {
     console.log("click test!");
   }
-
-
-  handleLogin()
 
   // modal
   const modal = document.querySelector(".modal");
@@ -48,7 +69,6 @@ contentDiv.addEventListener("click", (e) => {
   }
 
 });
-
 /*
   const modal = document.querySelector(".modal");
   const openModal = document.querySelectorAll(".card");
@@ -69,39 +89,3 @@ contentDiv.addEventListener("click", (e) => {
           modal.style.display = "none";
       }
   }*/
-
-const initLogin = () => {
-  const login = document.querySelector("#login");
-  const menu = document.querySelector("#menu");
-
-  login.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    // 로그인 중이면 로그 아웃하기
-
-    if (login.textContent === "Logout") {
-      logout();
-      return;
-    }
-
-    // 로그인이 아니라면 로그인 창 띄우기
-    window.open(
-      "./login.html",
-      "_black",
-      "width=300px,height=300px,toolbars=no,scrollbars=no"
-    );
-  });
-
-  menu.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (menu.textContent === "Add") {
-      window.location = "/edit";
-    }
-  });
-};
-
-const logout = () => {
-  login.textContent = "Login";
-  menu.textContent = "Join";
-  localStorage.removeItem("j2kb-accessToken");
-};
